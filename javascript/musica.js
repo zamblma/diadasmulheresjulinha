0fd73edc327379e1edc8886750c8e3404294b72a
Carlos Zambl <zamblma1@gmail.com>
2026-06-08 00:30:37 -0300

    player de musica com controle de volume e nome da faixa

--- javascript/musica.js
+++ javascript/musica.js
@@ -1,2 +1,13 @@
-var el = document.getElementById("bg-music");
-if (el) el.volume = 0.1;
+var audio = document.getElementById("bg-music");
+var slider = document.getElementById("player-volume");
+var label = document.getElementById("player-porcentagem");
+
+if (audio && slider && label) {
+    audio.volume = slider.value / 100;
+    label.textContent = slider.value + "%";
+
+    slider.addEventListener("input", function () {
+        audio.volume = this.value / 100;
+        label.textContent = this.value + "%";
+    });
+}
