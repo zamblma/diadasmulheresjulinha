56a8a751cba9a7d9e55a95b2a5f0313bf50a16f1
Carlos Zambl <zamblma1@gmail.com>
2026-06-08 00:29:09 -0300

    pag02 com musica baixada (namorados.mp3) em vez de YouTube API

--- javascript/musica.js
+++ javascript/musica.js
@@ -1,18 +1,2 @@
 var el = document.getElementById("bg-music");
-
-if (el && el.tagName === "AUDIO") {
-    el.volume = 0.1;
-} else if (el && el.tagName === "DIV") {
-    var tag = document.createElement("script");
-    tag.src = "https://www.youtube.com/iframe_api";
-    document.body.appendChild(tag);
-    var player;
-    function onYouTubeIframeAPIReady() {
-        player = new YT.Player("bg-music", {
-            height: "1", width: "1",
-            videoId: "X2BYmmTI04I",
-            playerVars: { autoplay: 1, loop: 1, playlist: "X2BYmmTI04I", controls: 0 },
-            events: { onReady: function (e) { e.target.setVolume(10); e.target.playVideo(); } }
-        });
-    }
-}
+if (el) el.volume = 0.1;
