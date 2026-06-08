7154c398ee63b5932afeef6066271db0d4bc359c
Carlos Zambl <zamblma1@gmail.com>
2026-06-08 00:22:34 -0300

    troca musica para YouTube API (monalisa -> X2BYmmTI04I)

--- javascript/musica.js
+++ javascript/musica.js
@@ -1,2 +1,26 @@
-const audio = document.getElementById("bg-music");
-audio.volume = 0.1;
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+document.body.appendChild(tag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player("bg-music", {
+        height: "1",
+        width: "1",
+        videoId: "X2BYmmTI04I",
+        playerVars: {
+            autoplay: 1,
+            loop: 1,
+            playlist: "X2BYmmTI04I",
+            controls: 0,
+            showinfo: 0
+        },
+        events: {
+            onReady: function (e) {
+                e.target.setVolume(10);
+                e.target.playVideo();
+            }
+        }
+    });
+}
